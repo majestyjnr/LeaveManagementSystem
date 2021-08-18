c76da4034e4055533bd7b6b063078fd1dee01fcc
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-08-18 13:44:39 -0700

    Commit Updates

---
+++ employee/profile.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicon.ico">
+    <title>Dashboard | Majesty Corporation LMS</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    
+</head>
+<?php 
+include "../db/connection.php";
+session_start();
+$name = $_SESSION['firstname'];
+$email = $_SESSION['email'];
+
+
+?>
+
+<body>
+    <div class="main-wrapper">
+        <div class="header">
+			<div class="header-left">
+				<a href="dashboard.php" class="logo">
+					<img src="../assets/img/logo.png" width="35" height="35"  alt=""> <span>LMS</span>
+				</a>
+			</div>
+            <a id="toggle_btn" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
+            <a id="mobile_btn" class="mobile_btn float-left" href="#sidebar"><i class="fa fa-bars"></i></a>
+            <ul class="nav user-menu float-right">
+             <li class="nav-item dropdown has-arrow">
+                    <a href="#" class="dropdown-toggle nav-link user-link" data-toggle="dropdown">
+                        <span class="user-img">
+							<img class="rounded-circle" src="../assets/img/account.png" width="24" alt="Admin">
+							<span class="status online"></span>
+						</span>
+						<span><?php echo $email ?></span>
+                    </a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="profile.php">My Profile</a>
+						<a class="dropdown-item" href="logout.php">Logout</a>
+					</div>
+                </li>
+            </ul>
+           
+        </div>
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">Main</li>
+                        <li class="active">
+                            <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        </li>
+						<li>
+                            <a href="request-leave.php"><i class="fa fa-user-md"></i> <span>Request Leave</span></a>
+                        </li>
+                        <li>
+                            <a href="leaves.php"><i class="fa fa-wheelchair"></i> <span>All Leaves</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="page-wrapper">
+            <div class="content">
+            <div class="row">
+                    <div class="col-sm-7 col-6">
+                        <h4 class="page-title">My Profile</h4>
+                    </div>
+
+                    <div class="col-sm-5 col-6 text-right m-b-30">
+                        <a href="edit-profile.php" class="btn btn-primary btn-rounded"><i class="fa fa-plus"></i> Edit Profile</a>
+                    </div>
+                </div>
+                <div class="card-box profile-header">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="profile-view">
+                                <div class="profile-img-wrap">
+                                    <div class="profile-img">
+                                        <a href="#"><img class="avatar" src="../assets/img/account.png" alt=""></a>
+                                    </div>
+                                </div>
+                                <div class="profile-basic">
+                                    <div class="row">
+                                        <div class="col-md-5">
+                                            <div class="profile-info-left">
+                                                <h3 class="user-name m-t-0 mb-0">Cristina Groves</h3>
+                                                <small class="text-muted">Staff</small>
+                                                <div class="staff-id">Employee ID : DR-0001</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-7">
+                                            <ul class="personal-info">
+                                                <li>
+                                                    <span class="title">Phone:</span>
+                                                    <span class="text"><a href="#">770-889-6484</a></span>
+                                                </li>
+                                                <li>
+                                                    <span class="title">Email:</span>
+                                                    <span class="text"><a href="#">cristinagroves@example.com</a></span>
+                                                </li>
+                                                <li>
+                                                    <span class="title">Birthday:</span>
+                                                    <span class="text">3rd March</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
+
+                <center>
+                <img src="../assets/img/mj.jpg" alt="" width="60%">
+                </center>
+            </div>
+        </div>
+    	
+	</div>
+    <div class="sidebar-overlay" data-reff=""></div>
+    <script src="../assets/js/jquery-3.2.1.min.js"></script>
+	<script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.slimscroll.js"></script>
+    <script src="../assets/js/Chart.bundle.js"></script>
+    <script src="../assets/js/chart.js"></script>
+    <script src="../assets/js/app.js"></script>
+
+</body>
+
+
+
+</html>
